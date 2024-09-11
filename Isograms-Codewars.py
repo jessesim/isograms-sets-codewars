d423522fae583a88f84185d386ed41a7789fe96f
Jesse <jessesimmons12@yahoo.com>
2024-09-11 12:17:26 -0400

    comments

--- Isograms-Codewars.py
+++ Isograms-Codewars.py
@@ -1,9 +1,12 @@
 def is_isogram(string):
     seen = set()
-    for char in string:
+    # iteratinig over each character and converting to each to lowercase for processing regardless of case
+    for char in string.lower():
+        # if a character is repeated in the set False is returned
         if char in seen:
             return False
         seen.add(char)
+    # if characters do not repeat returns true
     return True
 
 print (is_isogram ("betty"))
@@ -11,3 +14,4 @@ print (is_isogram ("tom"))
 print (is_isogram ("potato"))
 print (is_isogram ("bleach"))
 print (is_isogram (""))
+print (is_isogram ("MoMma"))
